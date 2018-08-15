93c521154e7db99efef8867dbef072679dfc22cb
Denys Samotos <38587580+denn1sam@users.noreply.github.com>
2018-08-15 14:22:12 +0300

    fixed bugs

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,11 +1,11 @@
-const moneySum = prompt(`Please enter amount of money`);
-const discout = prompt(`Please enter the discount`);
+const moneySum = parseFloat(prompt(`Please enter amount of money`));
+const discout = parseFloat(prompt(`Please enter the discount`));
 const priceWithDiscout = moneySum - moneySum * discout / 100;
 const saved = moneySum - priceWithDiscout;
 
 if(moneySum > 0 && discout > 0) {
-    console.log(`Price without discount: ` + moneySum + `\nDiscount: ` +
-    discout + `% \nPrice with discout: ` + +priceWithDiscout.toFixed(2) + 
+    console.log(`Price without discount: ` + +moneySum.toFixed(2) + `\nDiscount: ` +
+    +discout.toFixed(2) + `% \nPrice with discout: ` + +priceWithDiscout.toFixed(2) + 
     `\nSaved: ` + +saved.toFixed(2));
 } else {
     console.log(`Invalid data`);
